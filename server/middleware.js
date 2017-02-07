13ab1ba316cca01b587e8a56093831edab7d590e
Wallace <wallace.s.luk@gmail.com>
2017-02-07 03:18:55 -0800

    Allow custom headers

--- server/middleware.js
+++ server/middleware.js
@@ -1,8 +1,13 @@
+const bodyParser = require('body-parser');
+
 module.exports = (app, express) => {
+  //JSON body parser
+  app.use(bodyParser.json());
+
   //CORS Headers
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
     next();
   });
 };
