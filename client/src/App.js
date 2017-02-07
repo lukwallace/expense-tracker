055a265298c461c990d7d3fd96ff37eda3d25ac1
Wallace <wallace.s.luk@gmail.com>
2017-02-07 03:15:12 -0800

    Add expense dummy data, finish basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router';
+import { withRouter } from 'react-router';
+import { getToken, logOut } from './utils';
+import { post, getExpenses } from './Requests';
+import AddExpense from './AddExpense';
+import ExpenseList from './ExpenseList';
 import logo from './logo.svg';
 import './App.css';
 
+const dummy = {
+  _id: 'ohiu123huhida',
+  owner: 'Johnny',
+  datetime: new Date(),
+  amount: 20.25,
+  description: 'An exquisitely expensive sandwich'
+}
+
 class App extends Component {
+  state = {
+    token: '',
+    expenses: [],
+    datetime: '',
+    amount: '',
+    description: ''
+  }
+
   componentWillMount () {
+    const token = getToken();
+    if(token) {
+      this.setState({ token });
+    } else {
+      this.props.router.push('/login');
+    }
+  }
+
+  componentDidMount () {
+    getExpenses(this.state.token)
+    .then((expenses) => {
+      this.setState({ expenses });
+    });
+  }
+
+  handleLogOut = () => {
+    logOut();
     this.props.router.push('/login');
   }
 
@@ -15,8 +52,27 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
         </div>
         <div>
-          Hello World!
+          <AddExpense datetime={this.state.datetime} amount={this.state.amount} description={this.state.description}/>
+          <ExpenseList expenses={this.state.expenses} />
+          <ul>
+            <li> 
+              <div className="expense">
+                <h2> Joe <span> Tuesday 2:30 PM </span> </h2>
+                <div className="amount"> 20.25 </div>
+                <span> Delicious stufff </span>
+              </div>
+            </li>
+            <li> 
+              <div className="expense">
+                <h2> Joe <span> Tuesday 2:30 PM </span> </h2>
+                <div className="amount"> 20.25 </div>
+                <div className="description"> Delicious stuff </div>
+              </div>
+            </li>
+          </ul>
+
         </div>
+        <button className="logOut" onClick={this.handleLogOut}>Log out</button>
       </div>
     );
   }
